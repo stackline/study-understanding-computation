b7a036a4738e6db2bc7e556bf3b1e9544b14a79d
stackline <kyo10itimanben+noreply@gmail.com>
2018-10-14 20:31:17 +0900

    chapter 1.6 Finish reading

--- ch01.rb
+++ ch01.rb
@@ -4,8 +4,8 @@
 # [x] 1.2
 # [x] 1.3
 # [x] 1.4
-# [x] 1.5 p.25
-# [ ] 1.6
+# [x] 1.5
+# [x] 1.6
 
 puts '##### 1.1'
 
@@ -153,3 +153,172 @@ p AddingCalculator.include Addition
 ac = AddingCalculator.new
 p ac
 p ac.add(10, 2)
+
+puts "\n##### 1.6"
+
+p greeting = 'hello'
+p greeting
+
+# rubocop:disable Style/ParallelAssignment
+p((width, height, depth = [1000, 2250, 250]))
+# rubocop:enable Style/ParallelAssignment
+p width, height, depth
+
+p "hello #{'dlrow'.reverse}"
+
+o = Object.new
+p def o.to_s
+  'a new object'
+end
+p "here is #{o}"
+
+p o = Object.new
+p def o.inspect
+  '[my object]'
+end
+p o
+
+p x = 128
+p(
+  while x < 1000
+    puts "x is #{x}"
+    x *= 2
+  end
+)
+
+p def join_with_commas(*words)
+  words.join(', ')
+end
+p join_with_commas('one', 'two', 'three')
+
+# rubocop:disable Lint/DuplicateMethods
+p def join_with_commas(before, *words, after)
+  before + words.join(', ') + after
+end
+# rubocop:enable Lint/DuplicateMethods
+p join_with_commas('Testing: ', 'one', 'two', 'three', '.')
+
+p arguments = ['Testing: ', 'one', 'two', 'three', '.']
+p join_with_commas(*arguments)
+
+p((before, *words, after = ['Testing: ', 'one', 'two', 'three', '.']))
+p before
+p words
+p after
+
+p def do_three_times
+  yield
+  yield
+  yield
+end
+do_three_times { puts 'hello' }
+
+p def do_three_times_with_block_parameter
+  yield('first')
+  yield('second')
+  yield('third')
+end
+p(do_three_times_with_block_parameter { |n| puts "#{n}: hello" })
+
+p def number_names
+  [yield('one'), yield('two'), yield('three')].join(', ')
+end
+p(number_names { |name| name.upcase.reverse })
+
+p((1..10).count(&:even?))
+p((1..10).select(&:even?))
+p((1..10).any? { |number| number < 8 })
+p((1..10).all? { |number| number < 8 })
+p(
+  (1..5).each do |number|
+    if number.even?
+      puts "#{number} is even"
+    else
+      puts "#{number} is odd"
+    end
+  end
+)
+p((1..10).map { |number| number * 3 })
+p(%w[one two three].map(&:upcase))
+p(%w[one two three].map(&:chars))
+p(%w[one two three].flat_map(&:chars))
+p((1..10).inject(0) { |result, number| result + number })
+p((1..10).inject(1) { |result, number| result * number })
+p(%w[one two three].inject('Words:') { |result, word| "#{result} #{word}" })
+
+class Point < Struct.new(:x, :y) # rubocop:disable Style/StructInheritance
+  def +(other)
+    Point.new(x + other.x, y + other.y)
+  end
+
+  def inspect
+    "#<Point (#{x}, #{y})>"
+  end
+end
+p a = Point.new(2, 3)
+p b = Point.new(10, 20)
+p a + b
+p a.x
+p a.x = 35
+p a + b
+
+p Point.new(4, 5) == Point.new(4, 5) # rubocop:disable Lint/UselessComparison
+p Point.new(4, 5) == Point.new(6, 7)
+
+p class Point
+    def -(other)
+      Point.new(x - other.x, y - other.y)
+    end
+  end
+p Point.new(10, 15) - Point.new(1, 1)
+
+p class String
+    def shout
+      upcase + '!!!'
+    end
+  end
+p 'hello world'.shout
+
+p NUMBERS = [4, 8, 15, 16, 23, 42].freeze
+p NUMBERS
+
+p class Greetings
+    ENGLISH = ' hello'.freeze
+    FRENCH = 'bonjour'.freeze
+    GERMAN = 'guten Tag'.freeze
+  end
+p NUMBERS.last
+p Greetings::FRENCH
+
+p NUMBERS.last
+p Object.send(:remove_const, :NUMBERS)
+# p NUMBERS.last
+p Greetings::GERMAN
+p Object.send(:remove_const, :Greetings)
+# p Greetings::GERMAN
+
+p NUM_CONST = [1, 2, 3].freeze
+# p Object.send(:remove_const, :NUM_CONST)
+p Object.constants.grep(:NUM_CONST)
+
+# memo
+puts "\n##### memory reference"
+class Test; end
+test = Test.new
+p test
+p((test.object_id << 1).to_s(16))
+p((test.object_id * 2).to_s(16))
+
+puts "\n##### freeze"
+p test1 = [1].freeze
+p test1.frozen?
+
+# p test1[0] = 'foo'
+# p test1
+
+test2 = test1.dup
+p test2
+p test2.frozen?
+
+test2[0] = 'foo'
+p test2
