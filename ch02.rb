b079461c4f6717b9bd7afc726b7ec113626f0ec5
stackline <kyo10itimanben+noreply@gmail.com>
2018-11-05 00:31:12 +0900

    chapter 2.3.1.1 Do easy to look abstract syntax tree

--- ch02.rb
+++ ch02.rb
@@ -5,7 +5,7 @@
 # [x] 2.2
 # [ ] 2.3
 #   [ ] 2.3.1
-#   [ ] 2.3.1.1 p.23
+#   [ ] 2.3.1.1 p.24
 
 # reduce
 ret = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].reduce do |x, y|
@@ -14,16 +14,43 @@ end
 p ret
 
 # expression
+puts "\n### expression\n"
+
 class Number < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
 end
 
 class Add < Struct.new(:left, :right)
+  def to_s
+    "#{left} + #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
 end
 
 class Multiply < Struct.new(:left, :right)
+  def to_s
+    "#{left} * #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
 end
 
-pp Add.new(
+add = Add.new(
   Multiply.new(Number.new(1), Number.new(2)),
   Multiply.new(Number.new(3), Number.new(4))
 )
+# Internally call `#inspect` and call `#to_s` and expect a string.
+# ref. https://stackoverflow.com/questions/25488902/what-happens-when-you-use-string-interpolation-in-ruby/25491660?stw=2#25491660
+p add
+p Number.new(5)
